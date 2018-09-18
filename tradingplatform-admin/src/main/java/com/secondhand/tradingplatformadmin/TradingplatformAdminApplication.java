8660cf6a15b1e5ff5e9c8d2af300c87063ae453a
zhangjk <810797860@qq.com>
2018-09-18 11:41:42 +0800

    模块化

--- tradingplatform-admin/src/main/java/com/secondhand/tradingplatformadmin/TradingplatformAdminApplication.java
+++ tradingplatform-admin/src/main/java/com/secondhand/tradingplatformadmin/TradingplatformAdminApplication.java
@@ -1,12 +1,12 @@
-package com.secondhand.tradingplatform;
+package com.secondhand.tradingplatformadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TradingplatformApplication {
+public class TradingplatformAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TradingplatformApplication.class, args);
+		SpringApplication.run(TradingplatformAdminApplication.class, args);
 	}
 }
