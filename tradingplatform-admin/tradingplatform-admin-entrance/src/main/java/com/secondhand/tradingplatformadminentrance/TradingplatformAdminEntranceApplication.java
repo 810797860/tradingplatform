ef67904ec61d678e0d9a7038f06576ca669d1e9b
zhangjk <810797860@qq.com>
2018-09-29 17:38:54 +0800

    模块化-admin2

---
+++ tradingplatform-admin/tradingplatform-admin-entrance/src/main/java/com/secondhand/tradingplatformadminentrance/TradingplatformAdminEntranceApplication.java
@@ -0,0 +1,12 @@
+package com.secondhand.tradingplatformadminentrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingplatformAdminEntranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingplatformAdminEntranceApplication.class, args);
+	}
+}
